da199b5577d8eb38a8a22566ba2d81dff5be7e7e
Dsanchez05 <dsanchez24-0381@cca.edu.ph>
2025-04-24 20:23:35 +0800

    Add files via upload

---
+++ SQL File/MYSQL_Clause.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE MYSQL_Clause;
+USE MYSQL_Clause;
+
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    enrollment_limit INT NOT NULL,
+    students_enrolled INT NOT NULL
+);
+
+INSERT INTO courses (course_name, category, enrollment_limit, students_enrolled)
+VALUES
+    ('Data Science', 'Technology', 50, 45),
+    ('Graphic Design', 'Arts', 30, 25),
+    ('Digital Marketing', 'Business', 40, 35),
+    ('Introduction to Python', 'Technology', 100, 90),
+    ('Creative Writing', 'Literature', 25, 20),
+    ('UX/UI Design', 'Arts', 50, 40),
+    ('Project Management', 'Business', 60, 55),
+    ('Artificial Intelligence', 'Technology', 50, 48),
+    ('Public Speaking', 'Communication', 30, 28),
+    ('Photography', 'Arts', 25, 18),
+    ('Web Development', 'Technology', 75, 70),
+    ('SEO Strategies', 'Business', 30, 29),
+    ('Blockchain Basics', 'Technology', 50, 50),
+    ('Accounting 101', 'Business', 45, 40),
+    ('Film Editing', 'Arts', 35, 33),
+    ('Machine Learning', 'Technology', 60, 55),
+    ('Speech Writing', 'Communication', 20, 15),
+    ('Interior Design', 'Arts', 40, 38),
+    ('Leadership Training', 'Business', 50, 48),
+    ('Cybersecurity', 'Technology', 70, 65);
+ 
+ ## Task 1: Retrieve all students_enrolled < enrollment_limit ##
+SELECT * FROM courses
+WHERE students_enrolled < enrollment_limit;
+
+## Task 2: Group by categor, calculate total number of students enrolled ##
+SELECT category, sum(students_enrolled) AS total_enrolled
+FROM courses
+GROUP BY category;
+
+## Task 3: Retrieve students_enrolled = enrollment ##
+SELECT * FROM courses
+WHERE students_enrolled = enrollment_limit;
+
+## Task 4: Calculate total number of students enrolled ##
+SELECT SUM(students_enrolled) AS Total_students_enrolled
+FROM courses;
+
+## Task 5: Sort by students_enrolled in ascending order
+SELECT * FROM courses
+ORDER BY students_enrolled ASC;
